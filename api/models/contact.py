b2fb34f79de65a2dc5389fd26948de55d7dcf609
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-10-25 13:51:54 +0200

    Added subclassing for the Contact object

--- api/models/contact.py
+++ api/models/contact.py
@@ -1,11 +1,22 @@
 class Contact(object):
     contact_type = None
-    def __init__(self):
-        pass
+    is_public = False
+    def __init__(self, data):
+        public = data.get('is_public', False)
+        if isinstance(public, bool):
+            self.is_public = public
+
+    @staticmethod
+    def get_instance(contact_type, optional_args):
+        if contact_type == "email":
+            return EmailContact(optional_args)
+
+
 
 class EmailContact(Contact):
     email = None
 
-    def __init__(self, email):
-        self.email = email
+    def __init__(self, data):
+        super(EmailContact, self).__init__(data)
+        self.email = data.get('email', '')
         self.contact_type = "email"
